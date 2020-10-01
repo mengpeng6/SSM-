6c7e38a2d4dab87b14e60cd25b3b04d2a53c16e1
mengpeng6 <1085182848@qq.com>
2020-10-01 15:57:50 +0800

    这是使用IDEA修改的

--- pinyougou/Hello.java
+++ pinyougou/Hello.java
@@ -1,5 +1,6 @@
-public class A{
-          public static void main(String[] args){
-                 System.out.println("acsdacfa");
-}
+public class A {
+    public static void main(String[] args) {
+        System.out.println("acsdacfa");
+        System.out.println("你好世界");
+    }
 }
